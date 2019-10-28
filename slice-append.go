bbd0a95269e6ea3fd2fcb3800725d1027951aa8b
quanee <262504455@qq.com>
2019-10-28 13:02:03 +0800

    slice init different

---
+++ slice-append.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := make([]int, 5)
+	s = append(s, 1, 2, 3)
+	fmt.Println(s)
+	/*[0 0 0 0 0 1 2 3]*/
+
+	s = make([]int, 0, 5)
+	s = append(s, 1, 2, 3)
+	fmt.Println(s)
+	/*[1 2 3]*/
+}
