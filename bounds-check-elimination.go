c39f8340875dbbf7fee30550ab544f6682e2b78c
quanee <262504455@qq.com>
2019-10-28 13:41:13 +0800

    bounds check

---
+++ bounds-check-elimination.go
@@ -0,0 +1,45 @@
+package main
+
+func fd(is []int, bs []byte) {
+	if len(is) >= 256 {
+		for _, n := range bs {
+			_ = is[n] // 第7行： 需要边界检查
+		}
+	}
+}
+
+func fd2(is []int, bs []byte) {
+	if len(is) >= 256 {
+		is = is[:256] // 第14行： 一个暗示
+		for _, n := range bs {
+			_ = is[n] // 第16行： 边界检查消除了！
+		}
+	}
+}
+
+func fe(isa []int, isb []int) {
+	if len(isa) > 0xFFF {
+		for _, n := range isb {
+			_ = isa[n & 0xFFF] // 第24行： 需要边界检查
+		}
+	}
+}
+
+func fe2(isa []int, isb []int) {
+	if len(isa) > 0xFFF {
+		isa = isa[:0xFFF+1] // 第31行： 一个暗示
+		for _, n := range isb {
+			_ = isa[n & 0xFFF] // 第33行： 边界检查消除了！
+		}
+	}
+}
+
+func main() {}
+
+
+/*
+go build -gcflags="-d=ssa/check_bce/debug=1" bounds-check-elimination.go
+# command-line-arguments
+.\bounds-check-elimination.go:6:10: Found IsInBounds
+.\bounds-check-elimination.go:23:11: Found IsInBounds
+*/
