8d89531fd28da96dfd86ae50a6ee236f02a8d1cd
quanee <262504455@qq.com>
2019-12-01 09:18:09 +0800

    update qsort test

--- cgo/04qsort/qsort_test.go
+++ cgo/04qsort/qsort_test.go
@@ -1,22 +1,16 @@
 package qsort
 
 import (
-	"sort"
+	_sort "sort"
 	"testing"
-	"unsafe"
 )
 
 func TestSort(t *testing.T) {
 	values := []int32{42, 9, 101, 95, 27, 25}
 
-	Sort(unsafe.Pointer(&values[0]), len(values), int(unsafe.Sizeof(values[0])),
-		func(a, b unsafe.Pointer) int {
-			pa, pb := (*int32)(a), (*int32)(b)
-			return int(*pa - *pb)
-		},
-	)
+	Sort(values)
 
-	isSorted := sort.SliceIsSorted(values, func(i, j int) bool {
+	isSorted := _sort.SliceIsSorted(values, func(i, j int) bool {
 		return values[i] < values[j]
 	})
 	if !isSorted {
