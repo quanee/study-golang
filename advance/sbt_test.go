aeef2c3a3557d18e471b90948b0afbcc2390c6ce
quanee <262504455@qq.com>
2019-12-06 18:23:20 +0800

    float sort by int test

--- advance/sbt_test.go
+++ advance/sbt_test.go
@@ -1,6 +1,8 @@
-package advance
+package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSortFloat64FastV1(t *testing.T) {
 	var a = []float64{4, 2, 5, 7, 2, 1, 88, 1}
@@ -10,4 +12,13 @@ func TestSortFloat64FastV1(t *testing.T) {
 func TestSortFloat64FastV2(t *testing.T) {
 	var a = []float64{4, 2, 5, 7, 2, 1, 88, 1}
 	SortFloat64FastV2(a)
+}
+
+func BenchmarkSortFloat64FastV1(b *testing.B) {
+	var a = []float64{4, 2, 5, 7, 2, 1, 88, 1}
+
+	for i := 0; i < b.N; i++ {
+		SortFloat64FastV1(a)
+		//sort.Float64s(a)
+	}
 }
