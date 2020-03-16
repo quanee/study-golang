e9e88d3d444cf54f6fd5300f9fcc2e52acc17467
quanee <262504455@qq.com>
2020-03-16 14:14:42 +0800

    update

---
+++ leetcode/offer_11_recycle_min_num.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{1, 2, 3, 4, 5}
+	for i, v := range x[2:] {
+		fmt.Println(i, v)
+	}
+}
+
+func minArray(numbers []int) int {
+	for i, v := range numbers[1:] {
+		i = v * i
+		con
+	}
+	return 0
+}
